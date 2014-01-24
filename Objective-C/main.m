564ec277eba7bb55750522f3664bc44350c8e451
Phung Hoang Bac <phung.hoang.bac@framgia.com>
2014-01-24 11:57:00 +0700

    Initial Commit

---
+++ Objective-C/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Objective-C
+//
+//  Created by BacPH on 2014/01/24.
+//  Copyright (c) 2014年 Phung.Hoang.Bac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
